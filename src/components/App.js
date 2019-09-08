601720168717d2458d41c287f7ccf4db92873ede
Denis <ddujota@internationalsafety.com>
2019-09-08 13:03:59 -0400

    added semantic ui

--- src/components/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = () => <div className="ui container">App</div>;
-
-export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class PostList extends PureComponent {
+  render() {
+    return (
+      <div>
+        Post List
+      </div>
+    );
+  }
+}
+
+export default PostList;
