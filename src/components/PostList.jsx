17b084869284bef768f3cf3ac1e8ea483ab7e600
Denis <ddujota@internationalsafety.com>
2019-09-08 14:12:31 -0400

    added the rendering of the action creator

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,6 +3,11 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostList extends PureComponent {
+  componentDidMount() {
+    const { fetchPosts } = this.props;
+    fetchPosts();
+  }
+
   render() {
     return (
       <div>
