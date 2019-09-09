15e8de0fc2657fcbc36b88a0e4112bedd4fea543
Denis <ddujota@internationalsafety.com>
2019-09-08 22:01:02 -0400

    Added async action creators and redux thunk architecture

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,42 @@
-export const fetchPosts = () => ({
-  type: 'FETCH_POSTS',
-});
+/* eslint-disable import/prefer-default-export */
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+/* BAD APPROACH FOR REDUX BECAUSE IT RETURNS A PROMISE NOT AN OBJECT
+    const response = await jsonPlaceholder.get('/posts');
+
+    return {
+      type: 'FETCH_POSTS',
+      payload: response,
+    };
+
+
+  VERY COMMON ERROR :  Error: Actions must be plain objects. Use custom middleware for async actions
+*/
+
+
+/**
+   * ANOTHER BAD APPROACH WITH NO ASYNC
+   *
+   * THIS WILL RETURN A PLAIN OBJECT AND NOT CRASH, BUT IT WILL NOT SET THE NEW DATA
+   * SINCE IT NEEDS TO RESOLVE THE PROMISE
+   * const promise = jsonPlaceholder.get('/posts);
+   *
+   * return {
+   * type:'FETCH_POSTS',
+   * payload: promise
+   * }
+   *
+   * this does not throw an error message!!!!!
+   */
+
+/* Setup async Fetch by returning a function that has our promise
+  receives the dispatch and getState fn from redux to read and write redux store data.
+  we can now manually dispatch an action and update the data inside the store with this new action
+ */
+export const fetchPosts = () => async (dispatch) => {
+  // only modifies the value of the inner function. Thunk only calls the function, it is only the outer function that we care about in the action return
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response });
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// REDUX IMPORTS
+// REDUX
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+
+// THUNK
+import thunk from 'redux-thunk';
 
 // COMPONENTS
 import App from './components/App';
@@ -11,8 +15,12 @@ import App from './components/App';
 // REDUCERS
 import reducers from './reducers';
 
+// first import thunk and middleware fn and pass it to createStore, works for ALL MIDDLEWARE
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
 
